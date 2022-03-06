3f815270408600e1d42f75ff8546e698dfdd1026
Jessy Yeh <jiejun.yeh@gmail.com>
2022-03-06 20:50:37 +0000

    Add shuffle card function

--- code/script.js
+++ code/script.js
@@ -5,7 +5,7 @@ let firstCard, secondCard;
 
 function flipCard() {
   if (lockBoard) return;
-
+  if (this === firstCard) return;
   this.classList.add("flip");
 
   if (!hasFlippedCard) {
@@ -30,6 +30,8 @@ function checkForMatch() {
 function disableCards() {
   firstCard.removeEventListener("click", flipCard);
   secondCard.removeEventListener("click", flipCard);
+
+  resetBoard();
 }
 
 function unflipCards() {
@@ -37,8 +39,20 @@ function unflipCards() {
   setTimeout(() => {
     firstCard.classList.remove("flip");
     secondCard.classList.remove("flip");
-    lockBoard = false;
+    resetBoard();
   }, 1500);
 }
+
+function resetBoard() {
+  [hasFlippedCard, lockBoard] = [false, false];
+  [firstCard, secondCard] = [null, null];
+}
+
+(function shuffle() {
+  cards.forEach((card) => {
+    let randomPos = Math.floor(Math.random() * 12);
+    card.style.order = randomPos;
+  });
+})();
 
 cards.forEach((card) => card.addEventListener("click", flipCard));
